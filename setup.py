b32be70ba37ebd429bd225df88deadeaf0df68c8
kagut57 <156945786+kagut57@users.noreply.github.com>
2025-05-08 02:00:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,43 +1,43 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*
-
-from __future__ import absolute_import
+# -*- coding: utf-8 -*-
 
 import os
-from codecs import open
-
 from setuptools import find_packages, setup
 
-# allow setup.py to be run from any path
+# Allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+# Read requirements from the requirements.txt file
 with open('requirements.txt') as f:
     install_requires = f.read().splitlines()
 
+# Read README and HISTORY files
 with open('README.md', 'r', encoding='utf-8') as rm_file:
     readme = rm_file.read()
 
 with open('HISTORY.md', 'r', encoding='utf-8') as hist_file:
     history = hist_file.read()
 
-setup(name='mega.py',
-      version='1.0.9.dev0',
-      packages=find_packages('src', exclude=('tests', )),
-      package_dir={'': 'src'},
-      include_package_data=True,
-      zip_safe=False,
-      url='https://github.com/odwyersoftware/mega.py',
-      description='Python lib for the Mega.co.nz API',
-      long_description=readme + '\n\n' + history,
-      long_description_content_type='text/markdown',
-      author='O\'Dwyer Software',
-      author_email='hello@odwyer.software',
-      license='Creative Commons Attribution-Noncommercial-Share Alike license',
-      install_requires=install_requires,
-      classifiers=[
-          'Intended Audience :: Developers',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-          'Topic :: Internet :: WWW/HTTP',
-      ])
+setup(
+    name='mega.py',
+    version='1.0.57.dev0',
+    packages=find_packages('src', exclude=('tests', )),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    url='https://github.com/kagut57/mega.py/',
+    description='Python library for the Mega.co.nz API',
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
+    author="O'Dwyer Software",
+    author_email='hello@odwyer.software',
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    install_requires=install_requires,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
